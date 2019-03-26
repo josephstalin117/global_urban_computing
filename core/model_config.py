70ef4580fcadcb01adfda723d88ced444e828905
josephlin <josephlin117@hotmail.com>
2019-03-26 16:55:10 +0800

    edit config & gitignore

--- core/model_config.py
+++ core/model_config.py
@@ -5,7 +5,7 @@ csv = dict(
 model_setting = dict(
     model_in_columns=["inNums", "outNums", "week_day", "current_hour", "lag_yesterday_in", "lag_yesterday_out", "lag_last_week_in", "lag_last_week_out", "is_holiday"],
     model_out_columns=["inNUms", "outNums"],
-    epochs=1000,
+    epochs=4,
     n_in=144,
     n_out=144,
 )
